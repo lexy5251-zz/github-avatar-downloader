b8835e2c6f7236a288ac0582149bc5a7b0fa7d67
lexy5251 <xixi5251@gmail.com>
2018-08-07 21:03:38 +0000

    print avatar

--- download_avatars.js
+++ download_avatars.js
@@ -3,17 +3,29 @@ var request = require('request');
 console.log('Welcome to the GitHub Avatar Downloader!');
 
 function getRepoContributors(repoOwner, repoName, cb) {
-  var url = "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors";
+  var options =  {
+  url: "https://api.github.com/repos/" +
+  repoOwner + "/" +
+  repoName + "/contributors",
   headers: {
       'User-Agent': 'request',
-      'Authorization': 'secrets'
+      'Authorization': 'secrets.js'
     }
+  }
+
   request(options, function(err, res, body) {
+    // the callback function is so hard to understand
     cb(err, body);
+    var arr = JSON.parse(body);
+
+    for (var i = 0; i < arr.length; i++) {
+      console.log(arr[i].avatar_url);
+    }
   });
+
 }
 
 getRepoContributors("jquery", "jquery", function(err, result) {
   console.log("Errors:", err);
-  console.log("Result:", result);
+ // console.log("Result:", result);
 });
