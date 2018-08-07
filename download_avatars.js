345e6e3f3256194e476ea9fbd0ab53709632b464
lexy5251 <xixi5251@gmail.com>
2018-08-07 17:55:10 +0000

    secret file

--- download_avatars.js
+++ download_avatars.js
@@ -3,7 +3,14 @@ var request = require('request');
 console.log('Welcome to the GitHub Avatar Downloader!');
 
 function getRepoContributors(repoOwner, repoName, cb) {
-  // ...
+  var url = "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors";
+  headers: {
+      'User-Agent': 'request',
+      'Authorization': 'secrets'
+    }
+  request(options, function(err, res, body) {
+    cb(err, body);
+  });
 }
 
 getRepoContributors("jquery", "jquery", function(err, result) {
